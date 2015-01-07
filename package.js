ba7d1860e755d1d72c5bc2c88408f9d641fa991c
Kevin Green <kevin@swedenunlimited.com>
2015-01-07 16:36:28 -0500

    improper linking to css package

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-	name: 'orionjs:styles',
-	summary: 'Styles for orionjs admin',
+	name: 'iamkevingreen:styles-sass-orionjs',
+	summary: 'Styles for orionjs admin with sass',
 	version: '0.1.1',
-	git: 'https://github.com/orionjs/styles'
+	git: 'https://github.com/iamkevingreen/orionjs-sass-styles'
 });
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
-	api.use('less');
+	api.use(['fourseven:scss@1.0.0']);
 
 	api.addFiles([
-		'lib/global.less',
+		'lib/global.scss',
 		], 'client');
 });
 
