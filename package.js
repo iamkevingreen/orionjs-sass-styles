fa46d2076b7f5b6659ec3b9a95d75d7e0bb2035d
Kevin Green <kevin@swedenunlimited.com>
2015-01-09 12:14:57 -0500

    repeater form styles

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'iamkevingreen:orionjs-sass-styles',
 	summary: 'Styles for orionjs admin with sass',
-	version: '0.1.3',
+	version: '0.1.4',
 	git: 'https://github.com/iamkevingreen/orionjs-sass-styles'
 });
 
